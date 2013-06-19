e8a538e0d70f10e61e3f01050f3c08f4aaece957
Nicolo Fusi <nicolo.fusi@gmail.com>
2013-06-19 14:50:36 +0100

    fixed setup and manifest

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import Extension
 from Cython.Distutils import build_ext
 from setuptools import setup
 
-version_number = '0.5'
+version_number = '0.52'
 compile_flags = ["-O3"]
 
 if sys.platform != "darwin":
